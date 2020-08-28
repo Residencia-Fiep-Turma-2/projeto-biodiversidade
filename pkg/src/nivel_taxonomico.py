c271bed2b3fef771a7d076efd33ff7df970c8e6b
tgopedrosa <tgopedrosa@gmail.com>
2020-08-28 10:21:08 -0300

    Comentando o codigo

--- pkg/src/nivel_taxonomico.py
+++ pkg/src/nivel_taxonomico.py
@@ -2,6 +2,8 @@ from Bio import Bio
 
 def checaNivelTaxonomico():
     bio = Bio("../data/test.csv")
+    #Percorre a lista criada com os dados do CSV e pega os valores da coluna 14 (posicao 13 da lista) para criar a lista com os niveis taxonomico
+    #Começando da posicao 1 da lista, pois a primeira linha é o cabeçalho
     nivel_taxonomico = [x[13] for x in bio.data[1:]]
     return nivel_taxonomico
 
