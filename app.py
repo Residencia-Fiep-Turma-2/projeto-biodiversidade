b7f034f4d367d3d96506e34470592591d4ac1dfa
tgopedrosa <tgopedrosa@gmail.com>
2020-09-03 15:17:15 -0300

    Exemplo de funcionalidade

--- app.py
+++ app.py
@@ -2,18 +2,22 @@ from pkg_biodiversidade import Bio
 
 #Teste 1
 def teste():
-    bio1 = Bio.Bio("pkg/data/test.csv")
+    bio1 = Bio.Bio("pkg/data/test3.csv")
 
     #Funcionalidade 1
     print(bio1.media())
     #Funcionalidade 2
     print(bio1.checaNivelTaxonomico())
     #Funcionalidade 3 
+    #filtrandos os valores das colunas "Responsavel pelo registro" e "Data do evento"
     columns_list = ["Responsavel pelo registro", "Data do evento"]
-    print(bio1.select_columns(columns_list))
+    bio1.select_columns(columns_list)
+    #filtrando a linha de acordo com a coluna "Numero do registro no portal" com o valor de "262289"
     column = "Numero do registro no portal"
     value = "262289"
-    print(bio1.filter_rows(column, value))
+    bio1.filter_rows(column, value)
+    #acessando os valores filtrados
+    print(bio1.filtered_data)
     #Funcionalidade 4
     bio1.verifica_lat_long()
 
